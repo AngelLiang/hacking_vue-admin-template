c4c70a9b94657668b76ae6545c9f65ef3a1b8938
AngelLiang <pl01665077@163.com>
2019-02-28 09:33:36 +0800

    feat: 侧边栏路由导航标题添加本地化语言功能

--- src/lang/zh.js
+++ src/lang/zh.js
@@ -1,7 +1,11 @@
 export default {
   login: '登录',
   logout: '登出',
-  
+
+  route: {
+    user: '用户'
+  },
+
   usersView: {
     username: '用户名',
     createDatetime: '创建时间',
--- src/views/layout/components/Sidebar/SidebarItem.vue
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -1,32 +1,44 @@
 <template>
-  <div v-if="!item.hidden&&item.children" class="menu-wrapper">
+  <div v-if="!item.hidden&&item.children"
+       class="menu-wrapper">
 
-    <router-link v-if="hasOneShowingChild(item.children) && !onlyOneChild.children&&!item.alwaysShow" :to="resolvePath(onlyOneChild.path)">
-      <el-menu-item :index="resolvePath(onlyOneChild.path)" :class="{'submenu-title-noDropdown':!isNest}">
-        <svg-icon v-if="onlyOneChild.meta&&onlyOneChild.meta.icon" :icon-class="onlyOneChild.meta.icon"/>
-        <span v-if="onlyOneChild.meta&&onlyOneChild.meta.title" slot="title">{{ onlyOneChild.meta.title }}</span>
+    <router-link v-if="hasOneShowingChild(item.children) && !onlyOneChild.children&&!item.alwaysShow"
+                 :to="resolvePath(onlyOneChild.path)">
+      <el-menu-item :index="resolvePath(onlyOneChild.path)"
+                    :class="{'submenu-title-noDropdown':!isNest}">
+        <svg-icon v-if="onlyOneChild.meta&&onlyOneChild.meta.icon"
+                  :icon-class="onlyOneChild.meta.icon" />
+        <span v-if="onlyOneChild.meta&&onlyOneChild.meta.title"
+              slot="title">{{ generateTitle(onlyOneChild.meta.title) }}</span>
       </el-menu-item>
     </router-link>
 
-    <el-submenu v-else :index="item.name||item.path">
+    <el-submenu v-else
+                :index="item.name||item.path">
       <template slot="title">
-        <svg-icon v-if="item.meta&&item.meta.icon" :icon-class="item.meta.icon"/>
-        <span v-if="item.meta&&item.meta.title" slot="title">{{ item.meta.title }}</span>
+        <svg-icon v-if="item.meta&&item.meta.icon"
+                  :icon-class="item.meta.icon" />
+        <span v-if="item.meta&&item.meta.title"
+              slot="title">{{ generateTitle(item.meta.title) }}</span>
       </template>
 
-      <template v-for="child in item.children" v-if="!child.hidden">
-        <sidebar-item
-          v-if="child.children&&child.children.length>0"
-          :is-nest="true"
-          :item="child"
-          :key="child.path"
-          :base-path="resolvePath(child.path)"
-          class="nest-menu"/>
+      <template v-for="child in item.children"
+                v-if="!child.hidden">
+        <sidebar-item v-if="child.children&&child.children.length>0"
+                      :is-nest="true"
+                      :item="child"
+                      :key="child.path"
+                      :base-path="resolvePath(child.path)"
+                      class="nest-menu" />
 
-        <router-link v-else :to="resolvePath(child.path)" :key="child.name">
+        <router-link v-else
+                     :to="resolvePath(child.path)"
+                     :key="child.name">
           <el-menu-item :index="resolvePath(child.path)">
-            <svg-icon v-if="child.meta&&child.meta.icon" :icon-class="child.meta.icon"/>
-            <span v-if="child.meta&&child.meta.title" slot="title">{{ child.meta.title }}</span>
+            <svg-icon v-if="child.meta&&child.meta.icon"
+                      :icon-class="child.meta.icon" />
+            <span v-if="child.meta&&child.meta.title"
+                  slot="title">{{ generateTitle(child.meta.title) }}</span>
           </el-menu-item>
         </router-link>
       </template>
@@ -78,6 +90,14 @@ export default {
     },
     resolvePath(...paths) {
       return path.resolve(this.basePath, ...paths)
+    },
+    generateTitle(title) {
+      const hasKey = this.$te('route.' + title)
+      const translatedTitle = this.$t('route.' + title) // $t :this method from vue-i18n, inject in @/lang/index.js
+      if (hasKey) {
+        return translatedTitle
+      }
+      return title
     }
   }
 }
