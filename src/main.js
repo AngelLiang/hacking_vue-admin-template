88b68d9208ad88a707231db77d0495e8f772ba7c
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-06-02 10:12:48 +0800

    Added annotation

--- src/main.js
+++ src/main.js
@@ -1,27 +1,27 @@
-import Vue from 'vue'
+import Vue from 'vue';
 // 引入 ElementUI
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css';
 
-import App from './App'
-import router from './router'
-import store from './store'
-import '@/icons' // icon
-import '@/permission' // 权限
+import App from './App';
+import router from './router';
+import store from './store';
+import '@/icons'; // icon
+import '@/permission'; // 权限
 
-Vue.use(ElementUI)
+Vue.use(ElementUI);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   el: '#app',
 
   // 引入路由
-  router,   
+  router,
 
   // 引入store对象
   // 为了在 Vue 组件中访问 this.$store property，你需要为 Vue 实例提供创建好的 store。
   store,
   template: '<App/>',
   components: { App }
-})
+});
