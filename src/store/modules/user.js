6ca9aee3d3ac099979460f24f54e8494e6e04533
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-06-02 10:26:00 +0800

    Added annotation

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -2,12 +2,14 @@ import { login, logout, getInfo } from '@/api/user'
 import { getToken, setToken, removeToken } from '@/utils/auth'
 import { resetRouter } from '@/router'
 
+// 创建 state
 const state = {
   token: getToken(),
   name: '',
   avatar: ''
 }
 
+// 创建 mutation
 const mutations = {
   SET_TOKEN: (state, token) => {
     state.token = token
@@ -20,54 +22,70 @@ const mutations = {
   }
 }
 
+/*
+创建 action
+Action 函数接受一个与 store 实例具有相同方法和属性的 context 对象，
+因此你可以调用 context.commit 提交一个 mutation，
+或者通过 context.state 和 context.getters 来获取 state 和 getters。
+
+Action 通过 store.dispatch 方法触发
+
+    store.dispatch('increment')
+*/
 const actions = {
   // user login
   login({ commit }, userInfo) {
     const { username, password } = userInfo
     return new Promise((resolve, reject) => {
-      login({ username: username.trim(), password: password }).then(response => {
-        const { data } = response
-        commit('SET_TOKEN', data.token)
-        setToken(data.token)
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
+      login({ username: username.trim(), password: password })
+        .then(response => {
+          const { data } = response
+          commit('SET_TOKEN', data.token)
+          setToken(data.token)
+          resolve()
+        })
+        .catch(error => {
+          reject(error)
+        })
     })
   },
 
   // get user info
   getInfo({ commit, state }) {
     return new Promise((resolve, reject) => {
-      getInfo(state.token).then(response => {
-        const { data } = response
+      getInfo(state.token)
+        .then(response => {
+          const { data } = response
 
-        if (!data) {
-          reject('Verification failed, please Login again.')
-        }
+          if (!data) {
+            reject('Verification failed, please Login again.')
+          }
 
-        const { name, avatar } = data
+          const { name, avatar } = data
 
-        commit('SET_NAME', name)
-        commit('SET_AVATAR', avatar)
-        resolve(data)
-      }).catch(error => {
-        reject(error)
-      })
+          commit('SET_NAME', name)
+          commit('SET_AVATAR', avatar)
+          resolve(data)
+        })
+        .catch(error => {
+          reject(error)
+        })
     })
   },
 
   // user logout
   logout({ commit, state }) {
     return new Promise((resolve, reject) => {
-      logout(state.token).then(() => {
-        commit('SET_TOKEN', '')
-        removeToken()
-        resetRouter()
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
+      logout(state.token)
+        .then(() => {
+          commit('SET_TOKEN', '')
+          removeToken()
+          resetRouter()
+          resolve()
+        })
+        .catch(error => {
+          reject(error)
+        })
     })
   },
 
@@ -87,4 +105,3 @@ export default {
   mutations,
   actions
 }
-
